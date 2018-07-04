dc53ccc446cd2bab01b8543369a21ad6aea14fb1
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2018-07-04 12:44:14 +0530

    Add password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var data = {
     id: 10
 }
 
-var token = jwt.sign(data, 'abc12345');
-console.log(token);
-var decoded = jwt.verify(token, 'abc12345');
+var password = 'abc123';
 
-console.log('decoded', decoded);
+bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash);
+    });
+});
+
+var hashedPwd = '$2a$10$Nj0PxbFpcbKs9BUBERNH7uHovHgpVVCb1/UZp.ZHEbr1od9j7YmnO';
+
+bcrypt.compare(password, hashedPwd, (err, hash)=>{
+    console.log(hash);
+});
+
+// var token = jwt.sign(data, 'abc12345');
+// console.log(token);
+// var decoded = jwt.verify(token, 'abc12345');
+
+// console.log('decoded', decoded);
 // var msg = 'I am Aviral!.';
 
 // var hash = SHA256(msg).toString();
